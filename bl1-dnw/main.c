3305c4019e76eb9871f187aed4fdcada52b0274e
Jeff Kent <jeff@jkent.net>
2021-09-11 09:36:18 -0500

    alignment, stmia, larson for bl1-dnw
    * changed alignment for relocation to 16 byte
    * use stmia with 4 registers for relocation
    * changed alingment for bss to 16 byte
    * use stmia with 4 registers for clearing bss
    * implement larson scanner to show usb alive and waiting

--- bl1-dnw/main.c
+++ bl1-dnw/main.c
@@ -18,15 +18,37 @@
 
 #include <stdbool.h>
 
+#include <asm/io.h>
 #include <dnw.h>
 #include <udc.h>
+#include <s5pv210.h>
+#include <timer.h>
 
+void larson(void)
+{
+    int i;
+    void *gpio = (void *) ELFIN_GPIO_BASE;
+    writel((readl(gpio + GPJ2CON_OFFSET) & ~0x0000FFFF) | 0x00001111,
+            gpio + GPJ2CON_OFFSET);
+
+    while (true) {
+        for (i = 0; i <= 3; i++) {
+            writel((readl(gpio + GPJ2DAT_OFFSET) & ~0x0000000F) |
+                    (~(1 << i) & 0x0000000F), gpio + GPJ2DAT_OFFSET);
+            mdelay(100);
+
+        }
+        for (i = 3; i >= 0; i--) {
+            writel((readl(gpio + GPJ2DAT_OFFSET) & ~0x0000000F) |
+                    (~(1 << i) & 0x0000000F), gpio + GPJ2DAT_OFFSET);
+            mdelay(100);
+        }
+    }
+}
 
 void main(void)
 {
     udc_probe();
     udc_register_gadget(&dnw_gadget);
-
-    while (true)
-        ;
+    larson();
 }
